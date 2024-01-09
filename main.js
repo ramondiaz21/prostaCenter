df4a21966d0d74259f0b2812b495bb63c5324cb4
Ramón Díaz <jramondiaz96@gmail.com>
2024-01-09 00:45:09 -0600

    Added select and mail test

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.navbar-collapse.show').style.height = availableHeight + 'px';
 
   
+
+  
 });
 
 $(window).scroll(function() {
@@ -24,5 +26,34 @@ $(window).scroll(function() {
   }
   if ($(this).scrollTop() < 500) {
     $('.navbar').removeClass('altNav')
+  }
+});
+
+// Obtén el elemento select y los elementos de las sucursales
+const selectElement = document.getElementById("ubicacion");
+const sucursalElements = document.querySelectorAll(".sucursal-wrapper");
+const mapImages = document.querySelectorAll(".map-wrapper img");
+
+// Muestra la información de Colima por defecto
+document.getElementById("colima-address").style.display = "flex";
+document.getElementById("colima-map").style.display = "block";
+
+// Agrega un event listener para el cambio en el select
+selectElement.addEventListener("change", function() {
+  // Oculta todas las sucursales y las imágenes
+  sucursalElements.forEach(sucursal => {
+    sucursal.style.display = "none";
+  });
+  mapImages.forEach(image => {
+    image.style.display = "none";
+  });
+
+  // Muestra la sucursal correspondiente al valor seleccionado y la imagen correspondiente
+  const selectedValue = selectElement.value;
+  const selectedSucursal = document.getElementById(`${selectedValue}-address`);
+  const selectedImage = document.querySelector(`.map-wrapper .${selectedValue}`);
+  if (selectedSucursal && selectedImage) {
+    selectedSucursal.style.display = "flex";
+    selectedImage.style.display = "block";
   }
 });
