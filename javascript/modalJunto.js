33899c5ffbe408859e91bf4763568b964f4462cd
Ramón Díaz <jramondiaz96@gmail.com>
2024-01-17 17:56:47 -0600

    Modal toggle on resize screen

--- javascript/modalJunto.js
+++ javascript/modalJunto.js
@@ -3,43 +3,48 @@ document.addEventListener('DOMContentLoaded', function () {
   var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
   var modalBody = document.getElementById('modalBody');
 
-  // Verifica si el ancho de la pantalla es menor o igual a 991.98px
-  if (window.innerWidth <= 991.98) {
-    $('.card-servicios').on('click', function (event) {
-      event.preventDefault();
+  // Función para actualizar el contenido del modal de servicios
+  function updateServicesModal(event) {
+    event.preventDefault();
 
-      var servicioData = {
-        name: $(this).find('.card-servicios-title').text(),
-        position: $(this).find('.card-subtitle').text(),
-        content: [$(this).find('.card-text').text()],
-        socials: []
-      };
+    var servicioData = {
+      name: $(this).find('.card-servicios-title').text(),
+      position: $(this).find('.card-subtitle').text(),
+      content: [$(this).find('.card-text').text()],
+      socials: []
+    };
 
-      // Actualiza el contenido del modal de servicios
-      modalBody.innerHTML = `
-        <button type="button" class="only-mobile btn btn-secondary" data-bs-dismiss="modal">< Volver</button>
-        <div class="photo-wrapper-general">
-          <div class="photo-wrapper">
-            <img src="images/doctors.svg" alt="">
-          </div>
+    // Actualiza el contenido del modal de servicios
+    modalBody.innerHTML = `
+      <button type="button" class="only-mobile btn btn-secondary" data-bs-dismiss="modal">< Volver</button>
+      <div class="photo-wrapper-general">
+        <div class="photo-wrapper">
+          <img src="images/doctors.svg" alt="">
         </div>
-        <div class="info-wrapper">
-          <button type="button" class="only-desktop" data-bs-dismiss="modal">< Volver</button>
-          <h5 class="sub-subtitle">${servicioData.position}</h5>
-          <h6 class="position-text">${servicioData.content}</h6>
-        </div>
-      `;
+      </div>
+      <div class="info-wrapper">
+        <button type="button" class="only-desktop" data-bs-dismiss="modal">< Volver</button>
+        <h5 class="sub-subtitle">${servicioData.position}</h5>
+        <h6 class="position-text">${servicioData.content}</h6>
+      </div>`;
 
-      // Cierra cualquier instancia previa del modal
-      myModal.hide();
+    // Cierra cualquier instancia previa del modal
+    myModal.hide();
 
-      // Abre el modal
-      myModal.show();
-    });
+    // Abre el modal
+    myModal.show();
+  }
 
-    $('#exampleModal').on('hidden.bs.modal', function () {
-      $('#modalBody').empty();
-    });
+  // Verifica el ancho de la pantalla y actualiza el modal de servicios
+  function checkScreenSize() {
+    if (window.innerWidth <= 991.98) {
+      $('.card-servicios').off('click').on('click', updateServicesModal);
+      $('#exampleModal').on('hidden.bs.modal', function () {
+        $('#modalBody').empty();
+      });
+    } else {
+      $('.card-servicios').off('click'); // Desvincula el evento click si la pantalla es grande
+    }
   }
 
   // Captura el evento de cerrarse el modal
@@ -68,8 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
         <div class="social-icons-wrapper">
           ${generateSocialIcons(doctorData.socials)}
         </div>
-      </div>
-    `;
+      </div>`;
 
     // Cierra cualquier instancia previa del modal
     myModal.hide();
@@ -91,4 +95,8 @@ document.addEventListener('DOMContentLoaded', function () {
       </a>
     `).join('');
   }
+
+  // Verifica el tamaño de la pantalla al cargar y al cambiar su tamaño
+  checkScreenSize();
+  window.addEventListener('resize', checkScreenSize);
 });
